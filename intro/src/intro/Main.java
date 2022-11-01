52c639961888bbc727a3c5638c621bffd4e8ae43
Lyna <Lyna@DESKTOP-CDFE6I5>
2022-11-01 17:28:20 +0300

    Lecture1

--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -14,7 +14,7 @@ public class Main {
         int vade = 12;
 
         double dolarDun = 18.25;
-        double dolarBugun = 18.30;
+        double dolarBugun = 18.3;
 
         boolean dolarDustuMu = false;
 
