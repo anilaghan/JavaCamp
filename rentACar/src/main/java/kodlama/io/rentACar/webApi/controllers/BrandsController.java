e1cac127388aa6b7bedd5632a0778823a5851b8f
Lyna <Lyna@DESKTOP-CDFE6I5>
2022-11-02 23:11:33 +0300

    Lecture4,5

--- rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -3,11 +3,14 @@ package kodlama.io.rentACar.webApi.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 
 @RestController
 @RequestMapping("/api/brands")
@@ -21,9 +24,14 @@ public class BrandsController {
 	}
 
 	@GetMapping("/getall")
-	public List<Brand> getAll() {
+	public List<GetAllBrandsResponse> getAll() {
 		return brandService.getAll();
 
+	}
+	
+	@PostMapping("/add")
+	public void add(@RequestBody CreateBrandRequest createBrandRequest) {
+		this.brandService.add(createBrandRequest);
 	}
 
 }
